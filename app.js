d06cda72f0bf1d94c78a10fa142505e62fe36383
Porobe Alex <alexkeby@gmail.com>
2019-03-16 12:06:22 +0100

    create page model

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const messages = require('express-messages');
 
 //Connecting to database
 mongoose.connect(config.database);
@@ -14,16 +18,42 @@ db.once('open', function() {
 //Initialising the express app
 const app = express();
 
-//Setting up the port 
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log(`Server started on port ${port}`)
+//Setting up the template engine views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Setting public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body-parser middleware 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Sessions middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
+// Validator middleware
+
+//Express messages middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
 
 //Setting routes
 const pages = ('./routes/pages.js')
 const adminPages = ('./routes/admin_pages.js')
 
-//Setting up the template engine views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//Setting up the port 
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log(`Server started on port ${port}`)
+});
